b4b014cae0027c2755a3d2a78416d1c095df1125
fnva <fnva@gft.com>
2015-04-22 17:10:57 +0200

    Github api authentication

--- test/spec.js
+++ test/spec.js
@@ -21,6 +21,11 @@ describe('bower-adaptiveme', function () {
 		version: "3.0.0"
 	});
 	
+	github.authenticate({
+		type: "token",
+		token: "W4/j5NW+URP85LYVcTgLMxXRJtYN4bTS959V238oQ2uJNXJRTYAplhXijRf07CVoi4KJTHwDat1eK47rw2kC7tSrFM/wvf9Y8gluXZluXk1gbDzcHLEhRHh/dQIXvEXGEGguQxA26z+P5L/40hIB/R8vzdwZKTbc7mVu3g6zA24="
+	});
+	
 	github.repos.getTags({
 		user: 'AdaptiveMe',
 		repo: 'adaptive-arp-api',
